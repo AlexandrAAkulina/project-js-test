786792c62f1ad944a9d96dc99559b5eb2f8719ab
AlexandrAAkulina <icomfy1484@gmail.com>
2023-11-13 16:09:52 +0200

    try

--- support.js
+++ support.js
@@ -1,12 +1,12 @@
-import save_the_children from './img/save_the_children.png';
-import hope from './img/hope.png';
-import united_24 from './img/united_24.png';
-import medical_corps from './img/medical_corps.png';
-import med_sans_front from './img/med_sans_front.png';
-import razom from './img/razom.png';
-import against_hunger from './img/against_hunger.png';
-import world_vision from './img/world_vision.png';
-import serhiy_prytula from './img/serhiy_prytula.png';
+// import save_the_children from './img/save_the_children.png';
+// import hope from './img/hope.png';
+// import united_24 from './img/united_24.png';
+// import medical_corps from './img/medical_corps.png';
+// import med_sans_front from './img/med_sans_front.png';
+// import razom from './img/razom.png';
+// import against_hunger from './img/against_hunger.png';
+// import world_vision from './img/world_vision.png';
+// import serhiy_prytula from './img/serhiy_prytula.png';
 
 // import img from './img/*.png';
 
@@ -15,47 +15,47 @@ const supportFonds = [
     title: 'Save the Children',
     url:
       'https://www.savethechildren.net/what-we-do/emergencies/ukraine-crisis',
-    img: 'save_the_children',
+    img: './img/save_the_children.png',
   },
   {
     title: 'Project HOPE',
     url: 'https://www.projecthope.org/country/ukraine/',
-    img: 'hope',
+    img: './img/hope.png',
   },
   {
     title: 'UNITED24',
     url: 'https://u24.gov.ua/uk',
-    img: 'united_24',
+    img: './img/support3.png',
   },
   {
     title: 'International Medical Corps',
     url: 'https://internationalmedicalcorps.org/country/ukraine/',
-    img: 'medical_corps',
+    img: './img/medical_corps.png',
   },
   {
     title: 'Medicins Sans Frontieres',
     url: 'https://www.msf.org/ukraine',
-    img: 'med_sans_front',
+    img: './img/med_sans_front.png',
   },
   {
     title: 'RAZOM',
     url: 'https://www.razomforukraine.org/',
-    img: 'razom',
+    img: './img/razom.png',
   },
   {
     title: 'Action against hunger',
     url: 'https://www.actionagainsthunger.org/location/europe/ukraine/',
-    img: 'against_hunger',
+    img: './img/against_hunger.png',
   },
   {
     title: 'World vision',
     url: 'https://www.wvi.org/emergencies/ukraine',
-    img: 'world_vision',
+    img: './img/world_vision.png',
   },
   {
     title: 'Serhiy Prytula Charity Foundation',
     url: 'https://prytulafoundation.org/en',
-    img: 'serhiy_prytula',
+    img: './img/serhiy_prytula.png',
   },
 ]
 
